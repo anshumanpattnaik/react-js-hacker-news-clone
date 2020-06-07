b510f0852b1c7a5c7e25ff0df3b9e249a5d29c30
Anshuman Pattnaik <anshuman.pattnaik524@gmail.com>
2020-06-08 00:30:00 +0300

    Hacker news API protocol modified to HTTPS

--- src/client/actions/constants.js
+++ src/client/actions/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://hn.algolia.com/api/v1';
+export const BASE_URL = 'https://hn.algolia.com/api/v1';
 export const ITMES = '/items/';
 export const FETCH_NEWS_FEED = 'FETCH_NEWS_FEED';
 export const SET_UPVOTE_COUNT = 'SET_UPVOTE_COUNT';
